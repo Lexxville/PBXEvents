ac9585367f705c391422732bb879881e32f7b83d
Georgi Manev <gmanev@gmail.com>
2015-02-18 20:27:30 +0200

    update

--- pbxevents-routes/src/main/java/com/jjinterna/pbxevents/routes/logfile/LogfileLifecycleStrategySupport.java
+++ pbxevents-routes/src/main/java/com/jjinterna/pbxevents/routes/logfile/LogfileLifecycleStrategySupport.java
@@ -7,23 +7,19 @@ import org.apache.camel.support.LifecycleStrategySupport;
 public class LogfileLifecycleStrategySupport extends LifecycleStrategySupport {
 
 	private CamelContext context;
-	private String fileName;
-	private String routeId;
-
-	private Thread watcher = new Thread(new LogfileWatcher(fileName) {
-		@Override
-		public void onEntryDelete() throws Exception {
-			context.stopRoute(routeId);
-		}
-		@Override
-		public void onEntryCreate() throws Exception {
-			context.startRoute(routeId);
-		}
-	}, fileName);
-
-	public LogfileLifecycleStrategySupport(String routeId, String fileName) {
-		this.routeId = routeId;
-		this.fileName = fileName;
+	private Thread watcher;
+	
+	public LogfileLifecycleStrategySupport(final String routeId, String fileName) {
+		watcher = new Thread(new LogfileWatcher(fileName) {
+				@Override
+				public void onEntryDelete() throws Exception {
+					context.stopRoute(routeId);
+				}
+				@Override
+				public void onEntryCreate() throws Exception {
+					context.startRoute(routeId);
+				}
+			}, fileName);
 	}
 
 	@Override
